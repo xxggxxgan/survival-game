f60aebafc43f1b5ad9be7f78a999c0ae037d3618
xxggxxgan <xxggxxgan@gmail.com>
2018-01-03 14:45:26 +0800

    New changes

---
+++ Desktop/xxggxxgan-master/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+
+	Rigidbody rb;
+	public float speed;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+
+		Vector3 move = new Vector3 (h, 0, v);
+
+		rb.AddForce (move * speed);
+	}
+}
